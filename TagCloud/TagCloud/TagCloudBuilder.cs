b4e7bad4884a7f664cf8c3cca4963847ad821080
Sergey Avdonin <sunlf@yandex.ru>
2015-12-10 03:34:58 +0500

    added simple alpha algo

---
+++ TagCloud/TagCloud/TagCloudBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagCloud
+{
+	class TagCloudBuilder: ITagCloudBuilder
+	{
+		private readonly IWordsReader reader;
+		private readonly IAlgorithm algorithm;
+		private readonly IImageWriter writer;
+		public TagCloudBuilder(IWordsReader reader, IAlgorithm algorithm, IImageWriter writer)
+		{
+			this.reader = reader;
+			this.algorithm = algorithm;
+			this.writer = writer;
+		}
+		public void Build()
+		{
+			var words = reader.ReadWords();
+			var cloud = algorithm.BuildTagCloud(words);
+			writer.WriteImage(cloud);
+		}
+	}
+}
